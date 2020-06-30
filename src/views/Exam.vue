2e8af3486d1ad6c96f0e760762f8c2ac14121971
Ruslan Makarov <ruslan.makarov@gmail.com>
2020-06-30 12:18:48 +0700

    Vue.js part init commit

---
+++ src/views/Exam.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="about">
+		<h1>This is an Exam page</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Exam",
+
+	mounted() {
+		console.log("---" + this.$route.params.certSlug);
+	}
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="hello">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<h2>exam materials and practice tests for</h2>
+		<h1>IT certifications</h1>
+		<p>
+			Free actual exam questions & answers for major IT certifications
+		</p>
+		<p>
+			Convenient exam preparation desk
+		</p>
+	</div>
+</template>
